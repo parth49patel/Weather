255e1d9e14cabb54cce3888aec127b9f35b6660c
Parth Patel <4parth9@gmail.com>
2024-07-31 14:39:34 -0400

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Parth Patel on 2024-07-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Parth Patel on 2024-07-31.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
